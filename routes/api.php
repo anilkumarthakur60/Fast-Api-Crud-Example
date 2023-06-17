1e68d319cc2f3e9732fbae6d482d510e4523ace5
Anil <anilkumarthakur60@gmail.com>
2023-06-17 19:20:31 +0545

    update

--- routes/api.php
+++ routes/api.php
@@ -8,9 +8,9 @@ use App\Http\Controllers\TagController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
 
 
 Route::controller(TagController::class)
